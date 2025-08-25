0c6fe2c7d20163cb4b2bdbdc1bbcf7040d90a0f3
mrprofessor <rudra.kar@icloud.com>
2025-08-25 17:29:19 +1200

    Clean-up

--- captions.sh
+++
@@ -1,4 +0,0 @@
-python scripts/llm_captions.py --model gemma3
-python scripts/llm_captions.py --model llama4
-python scripts/llm_captions.py --model llava16
-python scripts/llm_captions.py --model qwen25
